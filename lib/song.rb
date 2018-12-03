9b1c5891d8a9ba8855ffbb892e296e21815c29e6
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-12-03 03:29:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    self.artist = artist
+  
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    music = self.new(name)
+    music.save
+    music
+  end
+end
